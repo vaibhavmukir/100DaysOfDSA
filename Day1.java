e232f9fecacca9bc1b72738b28833f309eed3ae2
Vaibhav Mukir <mukir2004@gmail.com>
2026-05-06 18:11:32 +0530

    Day 1 Complete

--- Day1.java
+++ Day1.java
@@ -1,5 +1,37 @@
-public class Day1 {
+/* public class Day1 {
     public static void main(String[] args) {
         System.out.println("Lest go Start DSA");
     }
+} */
+
+//----------------------------------------------------------------------------------------------
+
+/* public class Day1 {
+
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4,5};
+
+        System.err.println(arr[1]); //output 2
+
+    }
 }
+ */
+//-----------------------------------------------------------------------------------------------
+
+/* public class Day1 {
+
+    public static void main(String[] args) {
+        int[] arr=new int[5];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+        arr[3]=4;
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Array : "+i);
+        }
+
+    }
+} */
+
+//--------------------------------------------------------------------------------------------------    
